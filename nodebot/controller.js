600063a53c7b856f9518729a70028b22d6c840c3
Chelsea Shipp <Chelsea@Tombo.local>
2015-07-25 14:57:19 -0500

    fix(joystick): fixing issues with joystick - not 100% working

--- nodebot/controller.js
+++ nodebot/controller.js
@@ -4,6 +4,14 @@ var board = new five.Board({
   port: '/dev/cu.usbserial-A900cfzF'
 });
 
+var sleep = function (time, callback) {
+  var stop = new Date().getTime();
+  while(new Date().getTime() < stop + time) {
+    ;
+  }
+  callback();
+}
+
 board.on('ready', function() {
   var left = new five.Joystick({
     pins: ['A2', 'A0']
@@ -13,7 +21,15 @@ board.on('ready', function() {
     pins: ['A3', 'A1']
   });
 
-  
+  var printY = function () {
+    console.log('left  y = ' + left.y + '  ' + 'right y = ' + right.y);
+  }
+
+  for (var i = 0; i < 10; ++i) {
+    sleep(1000, function () {
+      console.log('left  y = ' + left.y + '  ' + 'right y = ' + right.y);
+    });
+  }
 
   // left.on('data', function () {
   //   console.log('left joystick');
